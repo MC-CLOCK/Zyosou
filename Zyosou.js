6afaaa113a1cde7e0fe5af0a100538e470810bb7
MC-CLOCK <tdk123@hotmail.co.jp>
2017-05-15 08:58:41 +0900

    少し真面目に
    Alertだと日本語の時バグるから Classで赤くするように変更
    WとLOLは正規表現とか言うの使い
    草とか全角ｗは半分手動で動くようにした

--- Zyosou.js
+++ Zyosou.js
@@ -1,14 +1,54 @@
-var Tb = document.getElementsByClassName('autosuggest-textarea__textarea');
+//マストドンのテクストボックスのクラス
+var TootTextBox = document.getElementsByClassName('autosuggest-textarea__textarea');
+//マストドンのTootボタンのクラス
+var TootButton = document.getElementsByClassName('button ');
 
-function OBWC() {
-    var input = Tb[0].value;
+var Alert = 0;//草が存在しているか
 
-    if (input.match('w' || 'W' /*|| '草' || 'ｗ' || 'Ｗ' || 'LOL' || 'lol' || 'ＬＯＬ' || 'ｌｏｌ' ||'大草原'*/)) {
+function IsThereGrass() {
+//*|ＬＯＬ|ｌｏｌ|*/
+    var TextBoxValue = TootTextBox[0].value;
 
-        alert('NG Word Check Please');
+    if (TextBoxValue.match(/w|lol/i)) {
 
+        Alert = 1;
+
+    }
+
+    if (TextBoxValue.match(/草/)) {
+
+        Alert = 1;
+
+    }
+
+    if (TextBoxValue.match(/ｗ/)) {
+
+        Alert = 1;
+
+    }
+
+    if (TextBoxValue.match(/Ｗ/)) {
+
+        Alert = 1;
+
+    }
+
+    if (Alert){
+
+        TootTextBox[0].style.color = "#ff0000";
+        Alert = 0;
+
+    } else {
+        TootTextBox[0].style.color = "#000000";
     }
 
 }
 
-Tb[0].addEventListener("input", function () { OBWC();},false);
+//function StopAlertOn0() {
+
+//}
+
+//TootTextBoxにイベントOnInputを追加
+TootTextBox[0].addEventListener("input", function () { IsThereGrass(); }, false);
+//TootButtonにイベントOnClickを追加
+//TootButton[0].addEventListener("click", function () { StopAlertOn0();},false);
